3fb4b1c559e352e9a43e15294cc6627373716ed9
notbroked <notbroked@gmail.com>
2025-05-05 22:12:06 -0400

    Improved button, and clicking, and art

--- scene/button.gd
+++ scene/button.gd
@@ -4,6 +4,15 @@ extends Node2D
 
 
 
-func _on_button_pressed() -> void:
-	print("clicked")
+
+
+
+func _on_button_button_down() -> void:
 	animated_sprite_2d.play("clicked")
+
+
+
+
+func _on_button_button_up() -> void:
+	print("clicked")
+	animated_sprite_2d.play("default")
